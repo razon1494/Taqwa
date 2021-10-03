535a18b091facc722d92195f93bdbc34257d39b4
razon1494 <razon1494@gmail.com>
2021-10-04 04:03:20 +0600

    Footer Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,17 @@
+import {faFacebook, faGoogle, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Footer.css'
 const Footer = () => {
     return (
-        <div>
-            <h2 id='footer-tag'>Footer Ending here</h2>
+        <div className='footer'>
+            <h2 id='footer-tag'>Taqwa Islamic Center</h2>
+            <h6>Contact Us: razon1494@gmail.com</h6>
+            <p>Mobile: 01744941494</p>
+            <FontAwesomeIcon className='footer-icon' icon={faTwitter} />
+           <FontAwesomeIcon className='footer-icon' icon={faFacebook} />
+           <FontAwesomeIcon className='footer-icon' icon={faYoutube} />
+            <FontAwesomeIcon className='footer-icon' icon={faGoogle} />
         </div>
     );
 };
